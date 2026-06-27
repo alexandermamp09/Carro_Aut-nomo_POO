397b3387a496cc39a2948ac4083bf0f42f98a8e6
alexandermamp09 <alexandermamp@gmail.com>
2026-06-27 16:31:37 -0400

    Refactor SensorToF and add ToFArray class
    Refactored SensorToF class and added ToFArray class for managing multiple sensors.

--- SensorToF.cpp
+++ SensorToF.cpp
@@ -1,30 +1,56 @@
-#include SensorToF.h
+#include "ToFArray.h"
 
-SensorToF: :SensorToF(int xshut) {
-    pinXshut = xshut;   
+ToFArray::ToFArray(uint8_t xshutPins[NUM_SENSORS])
+{
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        pinsXshut[i] = xshutPins[i];
+    }
 }
 
-bool SensorToF: :inicializar() {
-    pinMode(pinXshut, OUTPUT);
-    digitalWrite(pinXshut, HIGH); 
-    delay(10); 
-    return lox.begin();
-}
+void ToFArray::begin()
+{
+    Wire.begin();
 
-int SensorToF: :inicializar() {
-    piMode(pinXshut, OUTPUT);
-    digitalWrite(pinXshut, HIGH);
+    // apagar todos los sensores
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        pinMode(pinsXshut[i], OUTPUT);
+        digitalWrite(pinsXshut[i], LOW);
+    }
+
     delay(10);
-    return lox.begin();
+
+    // inicializar uno por uno
+    for (int i = 0; i < NUM_SENSORS; i++) {
+
+        digitalWrite(pinsXshut[i], HIGH);
+        delay(10);
+
+        if (!sensors[i].begin(addresses[i])) {
+            Serial.print("Error ToF ");
+            Serial.println(i);
+        }
+
+        sensors[i].setAddress(addresses[i]);
+    }
 }
 
-int SensorToF: :obtenerDistancia() {
+int ToFArray::read(int index)
+{
     VL53L0X_RangingMeasurementData_t measure;
-    lox.rangingTest(&measure, false); 
-    if (measure.RangeStatus != 4) { 
+
+    sensors[index].rangingTest(&measure, false);
+
+    if (measure.RangeStatus != 4) {
         return measure.RangeMilliMeter;
-    } else {
-        return -1; 
+    }
+
+    return -1;
+}
+
+void ToFArray::readAll(int distances[NUM_SENSORS])
+{
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        distances[i] = read(i);
     }
 }
 //lito :b
