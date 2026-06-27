a65d3a27c563cf8f45046526900b2a63c6af95bf
alexandermamp09 <alexandermamp@gmail.com>
2026-06-27 16:30:54 -0400

    Add ToFArray class for multiple sensors management
    Refactor sensor class to support multiple ToF sensors.

--- SensorToF.h
+++ SensorToF.h
@@ -1,16 +1,29 @@
-#ifndef SENSOR_TOF_H
-#define SENSOR_TOF_H
 
+#include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_VL53l0X.h>
+#include <Adafruit_VL53L0X.h>
 
-class SensorToF {
-    private:
-        Adafruit_VL53l0X lox;
+#define NUM_SENSORS 5
 
-    public:
-        SensorToF(int xshut);
-        bool inicializar();
-        int obtenerDistancia();
-        };
+class ToFArray {
+
+  private:
+
+    Adafruit_VL53L0X sensors[NUM_SENSORS];
+
+    uint8_t addresses[NUM_SENSORS] = {0x30, 0x31, 0x32, 0x33, 0x34};
+
+    uint8_t pinsXshut[NUM_SENSORS];
+
+  public:
+
+    ToFArray(uint8_t xshutPins[NUM_SENSORS]);
+
+    void begin();
+
+    int read(int index);
+
+    void readAll(int distances[NUM_SENSORS]);
+};
+
 #endif
