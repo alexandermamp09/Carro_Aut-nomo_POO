2b56c87fd143335f007df6fb90c6b3cb3263a8be
alexandermamp09 <alexandermamp@gmail.com>
2026-06-30 14:48:44 -0400

    Refactor ServoDireccion class for duty cycle conversion

--- ServoDirección.cpp
+++ ServoDirección.cpp
@@ -3,7 +3,7 @@
 ServoDireccion::ServoDireccion(int pinServo, int centroServo)
 {
     pin = pinServo;
-    centro = 45;
+    centro = centroServo;
     anguloActual = centro;
 
     anguloMin = 0;
@@ -15,27 +15,25 @@ ServoDireccion::ServoDireccion(int pinServo, int centroServo)
 
 void ServoDireccion::inicializar()
 {
-   const int canalPWM = 0;
-
-ledcSetup(canalPWM, 50, 16);
-ledcAttachPin(pin, canalPWM);
+    ledcAttach(pin, 50, 16);  // ESP32 3.x CORRECTO
     centrar();
 }
 
-int ServoDireccion::convertirMicrosegundos(int angulo)
+int ServoDireccion::anguloADuty(int angulo)
 {
     angulo = constrain(angulo, anguloMin, anguloMax);
 
     int pulso = map(angulo, anguloMin, anguloMax, pulsoMin, pulsoMax);
 
-    return (pulso * 65535) / 20000;
+    float duty = (float)pulso / 20000.0; 
+
+    return duty * 65535;
 }
 
 void ServoDireccion::fijarAngulo(int angulo)
 {
-    const int canalPWM = 0;
     anguloActual = constrain(angulo, anguloMin, anguloMax);
-    ledcWrite(canalPWM, convertirMicrosegundos(anguloActual));
+    ledcWrite(pin, anguloADuty(anguloActual));
 }
 
 void ServoDireccion::centrar()
